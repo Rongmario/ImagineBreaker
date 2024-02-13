2148660a8ff59fba18825228f74e2a0823f388f9
Ametsuchiru <ametsuchiru@protonmail.com>
2024-02-13 11:14:20 +0000

    Javadocs for public facing API methods

--- src/main/java/zone/rong/imaginebreaker/ImagineBreaker.java
+++ src/main/java/zone/rong/imaginebreaker/ImagineBreaker.java
@@ -32,44 +32,86 @@ public final class ImagineBreaker {
     private static final VarHandle CLASS$REFLECTION_DATA = retrieveReflectionData();
 
     /**
-     * Accessor to sun.misc.Unsafe
+     * Accessor to sun.misc.Unsafe.
      *
-     * @return instance of sun.misc.Unsafe
+     * @return instance of sun.misc.Unsafe.
      */
     public static Unsafe unsafe() {
         return UNSAFE;
     }
 
     /**
-     * Accessor to the trusted MethodHandles$Lookup
+     * Accessor to the trusted MethodHandles$Lookup.
      *
-     * @return instance of the trusted lookup
+     * @return instance of the trusted lookup.
      */
     public static MethodHandles.Lookup lookup() {
         return LOOKUP;
     }
 
+    /**
+     * Opens all modules within the boot ModuleLayer.
+     */
     public static void openBootModules() {
         openModuleLayer(ModuleLayer.boot());
     }
 
+    /**
+     * Opens all modules within the specified ModuleLayer
+     *
+     * @param layer module layer to have all of its modules opened
+     */
     public static void openModuleLayer(ModuleLayer layer) {
         layer.modules().forEach(ImagineBreaker::openModule);
     }
 
+    /**
+     * Opens a specific module
+     *
+     * @param module module to be opened
+     */
     public static void openModule(Module module) {
         MODULE$OPEN_PACKAGES.set(module, WorldRejector.INSTANCE);
     }
 
+    /**
+     * Disguises a class as having a module of a different class.
+     * Extremely useful when invoking caller-sensitive methods.
+     *
+     * <p>Consider {@link ImagineBreaker#disguiseAsModule(Class, Class, Runnable)}
+     *    if you want to revert the disguise. Use the runnable to run actions before the reversion</p>
+     *
+     * @param target      target class to have its module changed
+     * @param moduleClass class to have its module queried
+     */
     public static void disguiseAsModule(Class<?> target, Class<?> moduleClass) {
         var module = CLASS$MODULE.get(moduleClass);
         CLASS$MODULE.set(target, module);
     }
 
+    /**
+     * Disguises a class as having a different module.
+     * Extremely useful when invoking caller-sensitive methods.
+     *
+     * <p>Consider {@link ImagineBreaker#disguiseAsModule(Class, Module, Runnable)}
+     *    if you want to revert the disguise. Use the runnable to run actions before the reversion</p>
+     *
+     * @param target target class to have its module changed
+     * @param module module for the target class
+     */
     public static void disguiseAsModule(Class<?> target, Module module) {
         CLASS$MODULE.set(target, module);
     }
 
+    /**
+     * Disguises a class as having a different module.
+     * Extremely useful when invoking caller-sensitive methods.
+     * This method allows reversion of module for the target class after the runnable is ran.
+     *
+     * @param target      target class to have its module changed
+     * @param moduleClass module for the target class
+     * @param runnable    runnable to run before the class is reverted to having its module changed
+     */
     public static void disguiseAsModule(Class<?> target, Class<?> moduleClass, Runnable runnable) {
         var old = CLASS$MODULE.get(target);
         disguiseAsModule(target, moduleClass);
@@ -77,6 +119,15 @@ public final class ImagineBreaker {
         CLASS$MODULE.set(target, old);
     }
 
+    /**
+     * Disguises a class as having a different module.
+     * Extremely useful when invoking caller-sensitive methods.
+     * This method allows reversion of module for the target class after the runnable is ran.
+     *
+     * @param target   target class to have its module changed
+     * @param module   module for the target class
+     * @param runnable runnable to run before the class is reverted to having its module changed
+     */
     public static void disguiseAsModule(Class<?> target, Module module, Runnable runnable) {
         var old = CLASS$MODULE.get(target);
         disguiseAsModule(target, module);
@@ -84,6 +135,13 @@ public final class ImagineBreaker {
         CLASS$MODULE.set(target, old);
     }
 
+    /**
+     * Wipes {@link jdk.internal.reflect.Reflection#fieldFilterMap} as well as the
+     * {@link java.lang.Class#reflectionData} member in their respective classes, though this is not done for OpenJ9.
+     * This method should be called first to eliminate the default filters
+     * but since the method to register field filters is copy-on-write
+     * new filters may be added after-the-fact, meaning this method needs to be called again.
+     */
     public static void wipeFieldFilters() {
         if (!IS_OPEN_J9) {
             for (var clazz : ((Map<Class, Set>) REFLECTION$FIELD_FILTER_MAP.get()).keySet()) {
@@ -93,6 +151,13 @@ public final class ImagineBreaker {
         REFLECTION$FIELD_FILTER_MAP.setVolatile((Object) null);
     }
 
+    /**
+     * Wipes {@link jdk.internal.reflect.Reflection#methodFilterMap} as well as the
+     * {@link java.lang.Class#reflectionData} member in their respective classes, though this is not done for OpenJ9.
+     * This method should be called first to eliminate the default filters
+     * but since the method to register method filters is copy-on-write
+     * new filters may be added after-the-fact, meaning this method needs to be called again.
+     */
     public static void wipeMethodFilters() {
         if (!IS_OPEN_J9) {
             for (var clazz : ((Map<Class, Set>) REFLECTION$METHOD_FILTER_MAP.get()).keySet()) {

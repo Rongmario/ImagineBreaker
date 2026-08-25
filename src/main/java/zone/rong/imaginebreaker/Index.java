dea9483764b3e1b4aa881f235e22d7df67a50a3e
Rongmario <rongmario@proton.me>
2026-08-25 14:24:35 +0100

    Deprecate isOpenJ9

--- src/main/java/zone/rong/imaginebreaker/Index.java
+++ src/main/java/zone/rong/imaginebreaker/Index.java
@@ -1,6 +1,5 @@
 package zone.rong.imaginebreaker;
 
-import org.jspecify.annotations.NonNull;
 import zone.rong.imaginebreaker.api.ImagineBreaker;
 import zone.rong.imaginebreaker.impl.ImagineBreakerImpl;
 
@@ -10,7 +9,6 @@ public final class Index {
 
     private static final boolean isSemeru = System.getProperty("java.vm.vendor").toLowerCase(Locale.ENGLISH).contains("openj9");
 
-    @NonNull
     public static ImagineBreaker get() {
         return PrivilegeEnclave.IMPL;
     }
@@ -19,6 +17,9 @@ public final class Index {
         return isSemeru;
     }
 
+    /**
+     * @deprecated use {@link #isSemeru()}
+     */
     @Deprecated
     public static boolean isOpenJ9() {
         return isSemeru();
